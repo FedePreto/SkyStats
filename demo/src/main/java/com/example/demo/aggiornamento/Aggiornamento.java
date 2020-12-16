564ceff6d7c591045ef5ad9052b17acdbba4d176
Diego30090 <diego.vaccarini@gmail.com>
2020-12-16 18:05:00 +0100

    Modificato il file Aggiornamento.java, l'aggiornamento del db delle città verrà fatto tramite l'ultimo orario disponibile e reperibile nel db

--- demo/src/main/java/com/example/demo/aggiornamento/Aggiornamento.java
+++ demo/src/main/java/com/example/demo/aggiornamento/Aggiornamento.java
@@ -2,14 +2,22 @@ package com.example.demo.aggiornamento;
 
 import java.util.TimerTask;
 
-import com.example.demo.controller.Ricerca;
+import com.example.*;
 
 public class Aggiornamento extends TimerTask {
 
 	Ricerca r = new Ricerca();
+	Città città new Città();
+	Convertitore conv= new Convertitore();
 
-	public void run() {
-		r.salvataggio();
+	public void aggiorna(){
+		if (LocalTime.now()-città.data() >5 * 3600000)
+			timer.schedule(agg, conv.differenzaDiDate(LocalTime.now(),), 5 * 3600000);
+			else {
+			r.salvataggio();
+			timer.schedule(agg, 0, 5 * 3600000);
+		}
+		}
 	}
 
 }
