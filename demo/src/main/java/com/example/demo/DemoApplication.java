7acd618b0af9a2d5643f5a0af116298e489fa305
FedePreto <s1092769@users.noreply.github.com>
2020-12-24 15:50:44 +0100

    La classe è stata configurata all'avvio per poter utilizzare le interfacce grafiche

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		//Per usare le interfacce grafiche si è dovuto utilizzare questo builder di avviamento
+		 SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
+		    builder.headless(false).run(args);
+		
 	}
 
 }
