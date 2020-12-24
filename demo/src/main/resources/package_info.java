c8f3a5a25223f8be2fc069d76b7a25a03f8a83d4
FedePreto <s1092769@users.noreply.github.com>
2020-12-24 19:59:32 +0100

    Classe per la spiegazione delle modifiche in application.properties

---
+++ demo/src/main/resources/package_info.java
@@ -0,0 +1,6 @@
+/**
+ * All'interno dell'application.properties è stata aggiunta una proprietà che permette a
+ * a SpringBoot di utilizzare gson anziche jackson per serializzare gli oggetti in questo modo
+ * quando viene fatto il return di un JsonObject o qualsiali altro oggetto appartente alla libreria
+ * Gson, SpringBoot riuscirà a serializzare correttamente l'oggetto e visualizzarlo in PostMan 
+ */
