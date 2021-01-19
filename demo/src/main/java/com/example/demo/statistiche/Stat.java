595cb58b133afa9709c5dd01be1d74449181e670
FedePreto <s1092769@users.noreply.github.com>
2021-01-19 17:57:38 +0100

    -Aggiunta la temperatura a getMax e getMin

--- demo/src/main/java/com/example/demo/statistiche/Stat.java
+++ demo/src/main/java/com/example/demo/statistiche/Stat.java
@@ -29,7 +29,7 @@ public class Stat {
 	 * @return
 	 */
 	
-	public Double[][] getValues(ArrayList<Citta>c){
+	public static Double[][] getValues(ArrayList<Citta>c){
 		
 		Double[][] dati = new Double[3][c.size()];
 		
@@ -47,7 +47,7 @@ public class Stat {
 	 * @param c Lista delle citta dalle quali prelevare i dati
 	 * @param citta nome della citta da selezionare
 	 * @return array di valori di temperatura delle citta <b>citta</b> nell array <b>c</b>
-	 */
+	 *//*
 	public Double[] getValues(ArrayList<Citta>c , String citta) {
 		Convertitore conv = new Convertitore();
 		ArrayList<Double> val = new ArrayList<Double>();
@@ -70,7 +70,7 @@ public class Stat {
 			Double[] d = new Double[val.size()];
 				return val.toArray(d);
 		}
-	}
+	}*/
 	/**
 	 * Metodo che manda indietro i dati utili alla ricerca sottoforma di Array Double
 	 * @author Federico
@@ -80,18 +80,19 @@ public class Stat {
 	 * @param isPressione	Flag utilizzato per decidere di avere indietro o l'umidità oppure la pressione
 	 * @return Array di Double contenente i valori richiesti
 	 */
-	public Double[] getValues(ArrayList<Citta> c, String citta, boolean isPressione) {
+	public Double[] getValues(ArrayList<Citta> c, String citta, int scelta) {
 		Convertitore conv = new Convertitore();
 		ArrayList<Double> val = new ArrayList<Double>();
 		try {
 			int idCitta = Integer.parseInt(citta);
 			for (int i = 0; i < c.size(); i++) {
 				if (c.get(i).getId() == idCitta) {
-					if (isPressione) {
-						val.add(c.get(i).getPressione());
-					} else {
-						val.add(c.get(i).getUmidita());
+					switch(scelta) {
+					case 0: val.add(c.get(i).getPressione());
+					case 1: val.add(c.get(i).getUmidita());
+					case 2: val.add(c.get(i).getTemperatura());
 					}
+					
 				}
 			}
 			Double[] d = new Double[val.size()];
@@ -99,10 +100,10 @@ public class Stat {
 		} catch (NumberFormatException e) {
 			for (int i = 0; i < c.size(); i++) {
 				if (c.get(i).getNome().equals(citta)) {
-					if (isPressione) {
-						val.add(c.get(i).getPressione());
-					} else {
-						val.add(c.get(i).getUmidita());
+					switch(scelta) {
+					case 0: val.add(c.get(i).getPressione());
+					case 1: val.add(c.get(i).getUmidita());
+					case 2: val.add(c.get(i).getTemperatura());
 					}
 				}
 			}
@@ -136,8 +137,8 @@ public class Stat {
 	 * @param media	Media dei valori	
 	 * @return	Varianza
 	 */
-	public double getVarianza(ArrayList<Citta> c, boolean isPressione) {
-		Double[] val = getValues(c,c.get(0).getNome(),isPressione);
+	public double getVarianza(ArrayList<Citta> c, int  scelta) {
+		Double[] val = getValues(c,c.get(0).getNome(), scelta);
 		double media = getMedia(val);
 		
 		if(val==null) return 0;
@@ -152,7 +153,7 @@ public class Stat {
 	}
 	/**
 	 * Metodo che calcola la varianza dati i valori e la loro media
-	 * @author Nicolò
+	 * @author Federico
 	 * @param val valori da calcolarne la varianza
 	 * @return varianza dei valori
 	 */
@@ -176,55 +177,55 @@ public class Stat {
 	/**
 	 * Date in input 2 date il metodo cerca nel database le citta con i valori massimi per ogni parametro
 	 * @author Federico 
-	 * @param inizio Data di inizio del range di tempo
-	 * @param fine	Data di fine del range di tempo
+	 * @param  citta {@link ArrayList} di tipo {@link Citta} contenente tutte le citta filtrate su cui fare statistiche
 	 * @return JsonObject che contiene tutte le con i valori massimi nel database nel range di tempo indicato
 	 */
-	public JsonObject getMax(Date inizio, Date fine) {
-		Convertitore conv = new Convertitore();
-		ArrayList<Citta> citta = conv.JsonToCitta(inizio,fine);
-		
+	public JsonObject getMax(ArrayList<Citta> citta) {	
 	/*
 	  Array volto alla memorizzazione dei vari valori massimi con la seguente logica di indici:
-		0-Umidità massima
-		1-Pressione massima
-		2-Varianza di umidità massima
-		3-Varianza di pressione massima
+		0- Pressione massima
+		1- Umidità massima
+		2- Temperatura massima 
+		3- Varianza di pressione massima
+		4- Varianza di umidità massima
+		5- Varianza di temperatura massima
 	*/
-		double max_val[] = new double[4];
+		double max_val[] = new double[6];
 		
 	/*
 	   Array volto alla memorizzazione dei vari indici dell'array citta contenenti le citta cercate
 	   (Sfrutta la stessa logica di posizionamento dell'arrey precedente)	
 	 */
-		int max_index[] = new int[4];
+		int max_index[] = new int[6];
 		
 		//Ciclo for che permette di analizzare tutte le citta presenti nell'arrayList
-		for (int i = 0; i < citta.size(); i++) {	
-			/*
-			   L'istanziamento delle seguenti variabili è volto alla memorizzazione
-			   dei valori permettendo così di non dover svolgere gli stessi calcoli due volte 
-			   con il conseguente risparmio notevole di tempo in fase di esecuzione			 
-			 */
+		for (int i = 0; i < citta.size(); i++) {				
 			
-			//Permette di memorizzare il valore massimo di umidità e l'indice della città che lo contiene
-				
-			if ( citta.get(i).getUmidita() > max_val[0]) {
-				max_val[0] = citta.get(i).getUmidita();
-				System.out.println(citta.get(i).getData());
-				System.out.println(i);
+			//Permette di memorizzare il valore massimo di pressione e l'indice della citta che lo contiene
+			if (citta.get(i).getPressione() > max_val[0]) {				
+				max_val[0] = citta.get(i).getPressione();
 				max_index[0] = i;
 				}
-			//Permette di memorizzare il valore massimo di pressione e l'indice della citta che lo contiene
-			if (citta.get(i).getPressione()> max_val[1]) {				
-				max_val[1] = citta.get(i).getPressione();
+			//Permette di memorizzare il valore massimo di umidità e l'indice della città che lo contiene
+			if ( citta.get(i).getUmidita() > max_val[1]) {
+				max_val[1] = citta.get(i).getUmidita();
 				max_index[1] = i;
 				}
-		}		
-		Favoriti fav = new Favoriti();
+			//Permette di memorizzare il valore massimo di temeperatura e l'indice della citta che lo contiene
+			if (citta.get(i).getTemperatura() > max_val[2]) {				
+				max_val[2] = citta.get(i).getTemperatura();
+				max_index[2] = i;
+				}
+		}	
+		/* 
+		   Creo un oggetto favoriti per prendere i nomi di tutte le citta della lista in maniera da non dover
+		   calcolare la varianza più volte della stessa città nella lista(questo mi permette di risparmiare un sacco di tempo)		    
+		*/
+		Favoriti fav = new Favoriti(); 
 		ArrayList<String> favoriti = fav.getFavoriti();
-		double getVarU=0;
 		double getVarP=0;
+		double getVarU=0;
+		double getVarT=0;
 		//Crea una barra che permette all'utente di visualizzare lo stato di avanzamento
 			BarraProgresso m=new BarraProgresso(0,favoriti.size());  
 			m.setVisible(true);  
@@ -234,23 +235,30 @@ public class Stat {
 			 m.jb.setValue(i);
 			for(int j=0; j<citta.size(); j++) {
 				if(favoriti.get(i).equals(citta.get(j).getNome())) {
-					getVarU= getVarianza(citta,true);
 					//getVarP memorizza la varianza della pressione per una determinata citta
-					getVarP=getVarianza(citta,false);
+					getVarP = getVarianza(citta, 0);
+					getVarU = getVarianza(citta, 1);
+					getVarT = getVarianza(citta, 2);
 					break;
 				}
 			}
-					//Permette di memorizzare il valore massimo di varianza di Umidita e l'indice della città su cui è stato calcolato
-				if (getVarU > max_val[2]) {
-					max_val[2] = getVarU;
-					max_index[2] = i;
-					}
+			//Permette di memorizzare il valore massimo di varianza di pressione e l'indice della città su cui è stato calcolato
+			if (getVarP > max_val[3]) {
+				max_val[3] = getVarP;
+				max_index[3] = i;
+				}
+			//Permette di memorizzare il valore massimo di varianza di Umidita e l'indice della città su cui è stato calcolato
+			if (getVarU > max_val[4]) {
+				max_val[4] = getVarU;
+				max_index[4] = i;
+			}
+			//Permette di memorizzare il valore massimo di varianza di temperatura e l'indice della città su cui è stato calcolato
+			if (getVarT > max_val[5]) {
+				max_val[5] = getVarT;
+				max_index[5] = i;
+				}
 				
-				//Permette di memorizzare il valore massimo di varianza di pressione e l'indice della città su cui è stato calcolato
-				if (getVarP > max_val[3]) {
-					max_val[3] = getVarP;
-					max_index[3] = i;
-					}
+				
 		}
 		m.dispose();
 		JsonObject JsonReturn = new JsonObject();
@@ -258,22 +266,30 @@ public class Stat {
 			JsonReturn.addProperty("Nessun valore trovato nel range di tempo specficato","");
 			return JsonReturn;
 		}
-		JsonObject Max_Umidity = new JsonObject();
 		JsonObject Max_Pression = new JsonObject();
-		JsonObject Max_Var_Um = new JsonObject();
+		JsonObject Max_Umidity = new JsonObject();
+		JsonObject Max_Temperature = new JsonObject();
 		JsonObject Max_Var_Pr = new JsonObject();
-		Max_Umidity.addProperty("Nome", citta.get(max_index[0]).getNome());
-		Max_Umidity.addProperty("Valore", new DecimalFormat("#.##").format(max_val[0])+"%");
-		Max_Pression.addProperty("Nome",citta.get(max_index [1]).getNome());
-		Max_Pression.addProperty("Valore", new DecimalFormat("#.##").format(max_val[1])+" hPa");
-		Max_Var_Um.addProperty("Nome", citta.get(max_index [2]).getNome());
-		Max_Var_Um.addProperty("Valore", new DecimalFormat("#.##").format(max_val[2]));
+		JsonObject Max_Var_Um = new JsonObject();
+		JsonObject Max_Var_Temp = new JsonObject();
+		Max_Pression.addProperty("Nome",citta.get(max_index [0]).getNome());
+		Max_Pression.addProperty("Valore", new DecimalFormat("#.##").format(max_val[0])+" hPa");
+		Max_Umidity.addProperty("Nome", citta.get(max_index[1]).getNome());
+		Max_Umidity.addProperty("Valore", new DecimalFormat("#.##").format(max_val[1])+"%");
+		Max_Temperature.addProperty("Nome",citta.get(max_index [2]).getNome());
+		Max_Temperature.addProperty("Valore", new DecimalFormat("#.##").format(max_val[2])+"°C");	
 		Max_Var_Pr.addProperty("Nome", citta.get(max_index [3]).getNome());
 		Max_Var_Pr.addProperty("Valore", new DecimalFormat("#.##").format(max_val[3]));
-		JsonReturn.add("Umidità Massima", Max_Umidity);
+		Max_Var_Um.addProperty("Nome", citta.get(max_index [4]).getNome());
+		Max_Var_Um.addProperty("Valore", new DecimalFormat("#.##").format(max_val[4]));
+		Max_Var_Temp.addProperty("Nome", citta.get(max_index [5]).getNome());
+		Max_Var_Temp.addProperty("Valore", new DecimalFormat("#.##").format(max_val[5]));
 		JsonReturn.add("Pressione Massima", Max_Pression);
-		JsonReturn.add("Varianza di Umidità Massima", Max_Var_Um);
+		JsonReturn.add("Umidità Massima", Max_Umidity);
+		JsonReturn.add("Temperatura Massima", Max_Temperature);
 		JsonReturn.add("Varianza di Pressione Massima", Max_Var_Pr);
+		JsonReturn.add("Varianza di Umidità Massima", Max_Var_Um);
+		JsonReturn.add("Varianza di Temperatura Massima", Max_Var_Temp);
 		return JsonReturn;
 	}
 	/**
@@ -283,51 +299,55 @@ public class Stat {
 	 * @param fine Data di fine del range
 	 * @return JsonObject che contiente tutte le citta con i valori minimi nel database contenute nel range di tempo definito da inizio e fine
 	 */
-	public JsonObject getMin(Date inizio, Date fine) {
-		Convertitore conv = new Convertitore();
-		ArrayList<Citta> citta = conv.JsonToCitta(inizio,fine);
-	/*
-	  Array volto alla memorizzazione dei vari valori minimi con la seguente logica di indici:
-		0-Umidità minima
-		1-Pressione minima
-		2-Varianza di umidità minima
-		3-Varianza di pressione minima
-	*/
-		double min_val[] = new double[4];
+	public JsonObject getMin(ArrayList<Citta>citta) {
+		/*
+		  Array volto alla memorizzazione dei vari valori massimi con la seguente logica di indici:
+			0- Pressione minima
+			1- Umidità minima
+			2- Temperatura minima 
+			3- Varianza di pressione minima
+			4- Varianza di umidità minima
+			5- Varianza di temperatura minima
+		*/
+		double min_val[] = new double[6];
 		
 	/*
 	   Array volto alla memorizzazione dei vari indici dell'array citta contenenti le citta cercate
 	   (Sfrutta la stessa logica di posizionamento dell'arrey precedente)	
 	 */
-		int min_index[] = new int[4];
+		int min_index[] = new int[6];
  //Inizializza i minimi al primo valore letto nell'array poi procederà con i confronti
-		 min_val[0] = citta.get(0).getUmidita();
-	     min_val[1] = citta.get(0).getPressione();
-	     min_val[2] = getVarianza(getValues(citta, citta.get(0).getNome()));
-		 min_val[3] = getVarianza(getValues(citta, citta.get(0).getNome()));
+		 min_val[0] = citta.get(0).getPressione();
+	     min_val[1] = citta.get(0).getUmidita();
+	     min_val[2] = citta.get(0).getTemperatura();
+	     min_val[3] = getVarianza(citta,0);
+		 min_val[4] = getVarianza(citta,1);
+		 min_val[5] = getVarianza(citta,2);
 		 //Ciclo for che permette di analizzare tutte le citta presenti nell'arrayList
 		for (int i = 1; i < citta.size(); i++) {
-		   /*
-			   L'istanziamento delle seguenti variabili è volto alla memorizzazione
-			   dei valori permettendo così di non dover svolgere gli stessi calcoli due volte 
-			   con il conseguente risparmio notevole di tempo in fase di esecuzione			 
-			 */				
-				//Permette di memorizzare il valore minimo di umidità e l'indice della città che lo contiene
-				if ( citta.get(i).getUmidita() < min_val[0]) {
-					min_val[0] = citta.get(i).getUmidita();
-					min_index[0] = i;
-				}
+			//Permette di memorizzare il valore minimo di pressione e l'indice della citta che lo contiene
+			if (citta.get(i).getPressione() < min_val[0]) {
+				min_val[0] = citta.get(i).getPressione();
+				min_index[0] = i;
+			}	
+			//Permette di memorizzare il valore minimo di umidità e l'indice della città che lo contiene
+			if ( citta.get(i).getUmidita() < min_val[1]) {
+				min_val[1] = citta.get(i).getUmidita();
+				min_index[1] = i;
+			}
+			//Permette di memorizzare il valore minimo di temperatura e l'indice della città che lo contiene
+			if ( citta.get(i).getTemperatura() < min_val[2]) {
+				min_val[2] = citta.get(i).getTemperatura();
+				min_index[2] = i;
+			}
 				
-				//Permette di memorizzare il valore minimo di pressione e l'indice della citta che lo contiene
-				if (citta.get(i).getPressione() < min_val[1]) {
-					min_val[1] = citta.get(i).getPressione();
-					min_index[1] = i;
-				}
+				
 		}
 		Favoriti fav = new Favoriti();
 		ArrayList<String> favoriti = fav.getFavoriti();
-		double getVarU=0;
 		double getVarP=0;
+		double getVarU=0;
+		double getVarT=0;
 		//Crea una barra che permette all'utente di visualizzare lo stato di avanzamento
 			BarraProgresso m=new BarraProgresso(0,favoriti.size());  
 			m.setVisible(true);  
@@ -337,46 +357,60 @@ public class Stat {
 			m.jb.setValue(i);
 			for(int j=1; j<citta.size(); j++) {
 				if(favoriti.get(i).equals(citta.get(j).getNome())) {					
-					getVarU= getVarianza(getValues(citta, citta.get(j).getNome(), false));
 					//getVarP memorizza la varianza della pressione per una determinata citta
-					getVarP=getVarianza(getValues(citta, citta.get(j).getNome(), true));
+					getVarP = getVarianza(citta,0);
+					getVarU = getVarianza(citta,1);
+					getVarT = getVarianza(citta,2);					
 					break;
 			   }
 			}
+			//Permette di memorizzare il valore minimo di varianza di pressione e l'indice della città su cui è stato calcolato
+			if (getVarP < min_val[3]) {
+				min_val[3] = getVarP;
+				min_index[3] = i;
 				//Permette di memorizzare il valore minimo di varianza di Umidita e l'indice della città su cui è stato calcolato
-				if (getVarU < min_val[2]) {					
-					min_val[2] = getVarU;
-					min_index[2] = i;
+				if (getVarU < min_val[4]) {					
+					min_val[4] = getVarU;
+					min_index[4] = i;
 				}
-				
 				//Permette di memorizzare il valore minimo di varianza di pressione e l'indice della città su cui è stato calcolato
-				if (getVarP < min_val[3]) {
-					min_val[3] = getVarP;
-					min_index[3] = i;
+				if (getVarT < min_val[5]) {
+					min_val[5] = getVarT;
+					min_index[5] = i;
+				
 				}				
 			}
+		}
 		m.dispose();
 		JsonObject JsonReturn = new JsonObject();
 		if(min_val[0] == 0 && min_val[1] == 0 && min_val[2] == 0 && min_val[3]==0) {
 			JsonReturn.addProperty("Nessun valore trovato nel range di tempo specficato","");
 			return JsonReturn;
 		}
-		JsonObject Min_Umidity = new JsonObject();
 		JsonObject Min_Pression = new JsonObject();
-		JsonObject Min_Var_Um = new JsonObject();
+		JsonObject Min_Umidity = new JsonObject();
+		JsonObject Min_Temperature = new JsonObject();
 		JsonObject Min_Var_Pr = new JsonObject();
-		Min_Umidity.addProperty("Nome", citta.get(min_index[0]).getNome());
-		Min_Umidity.addProperty("Valore", new DecimalFormat("#.##").format(min_val[0])+"%");
-		Min_Pression.addProperty("Nome",citta.get(min_index [1]).getNome());
-		Min_Pression.addProperty("Valore", new DecimalFormat("#.##").format(min_val[1])+" hPa");
-		Min_Var_Um.addProperty("Nome", citta.get(min_index [2]).getNome());
-		Min_Var_Um.addProperty("Valore", new DecimalFormat("#.##").format(min_val[2]));
+		JsonObject Min_Var_Um = new JsonObject();
+		JsonObject Min_Var_Temp = new JsonObject();
+		Min_Pression.addProperty("Nome",citta.get(min_index [0]).getNome());
+		Min_Pression.addProperty("Valore", new DecimalFormat("#.##").format(min_val[0])+" hPa");
+		Min_Umidity.addProperty("Nome", citta.get(min_index[1]).getNome());
+		Min_Umidity.addProperty("Valore", new DecimalFormat("#.##").format(min_val[1])+"%");
+		Min_Temperature.addProperty("Nome", citta.get(min_index[2]).getNome());
+		Min_Temperature.addProperty("Valore", new DecimalFormat("#.##").format(min_val[2])+"°C");
 		Min_Var_Pr.addProperty("Nome", citta.get(min_index [3]).getNome());
 		Min_Var_Pr.addProperty("Valore", new DecimalFormat("#.##").format(min_val[3]));
-		JsonReturn.add("Umidità Minima", Min_Umidity);
+		Min_Var_Um.addProperty("Nome", citta.get(min_index [4]).getNome());
+		Min_Var_Um.addProperty("Valore", new DecimalFormat("#.##").format(min_val[4]));
+		Min_Var_Temp.addProperty("Nome", citta.get(min_index [5]).getNome());
+		Min_Var_Temp.addProperty("Valore", new DecimalFormat("#.##").format(min_val[5]));
 		JsonReturn.add("Pressione Minima", Min_Pression);
-		JsonReturn.add("Varianza di Umidità Minima", Min_Var_Um);
+		JsonReturn.add("Umidità Minima", Min_Umidity);
+		JsonReturn.add("Temperatura Minima", Min_Var_Temp);
 		JsonReturn.add("Varianza di Pressione Minima", Min_Var_Pr);
+		JsonReturn.add("Varianza di Umidità Minima", Min_Var_Um);
+		JsonReturn.add("Varianza di Temperatura Minima", Min_Var_Temp);
 		return JsonReturn;
 	}
 	
@@ -388,7 +422,7 @@ public class Stat {
 	 * @param inizio data di inizio del range di tempo
 	 * @param fine data di fine del range di tempo
 	 * @return stringa di output
-	 */
+	 *//*
 	public String printMaxValues(Date inizio, Date fine) {
 		Convertitore conv = new Convertitore();
 		ArrayList<Citta> citta = conv.JsonToCitta(inizio,fine);
@@ -479,7 +513,7 @@ public class Stat {
 				"La città con pressione minore è: " + citta.get(minP).getNome() + " con " + min[1] + " hPa<br>"+
 				"La citta con varianza di pressione minore è: " + citta.get(minVP).getNome() + " con " + min[3];
 				
-	}
+	}*/
 	
 	/**
 	 * Date le città e la posizione geografica da filtrare ritorna un array di dati in base a <b>flag</b>
